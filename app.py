6a888e8ceb464af7d950a454b574095c96906085
root <root@DESKTOP-R8M6KQ1.localdomain>
2024-12-29 19:06:32 -0500

    implement

--- app.py
+++ app.py
@@ -1,6 +1,32 @@
-from flask import Flask
+from flask import Flask, request
+import json
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+from db import (
+    get_games as get_games_from_db,
+    place_bet as place_bet_in_db
+)
+
+@app.route('/get_games', methods = ["GET"])
+def get_games():
+    games = get_games_from_db()
+    ret = []
+    for game in games:
+        ret.append({
+            "game_id": game[0],
+            "home_team": game[1],
+            "away_team": game[2],
+            "start_time": game[3]
+        })
+    return ret
+
+@app.route('/place_bet', methods = ["POST"])
+def place_bet():
+    data = request.get_json()
+    game_id = data.get("game_id")
+    bet_amount = data.get("bet_amount")
+    bet_on = data.get("bet_on")
+    place_bet_in_db(game_id, bet_amount, bet_on)
+    return app.response_class(
+        status=200,
+    )
---
+++ db.py
@@ -0,0 +1,33 @@
+import os
+from dotenv import load_dotenv
+import psycopg2
+
+load_dotenv()
+
+class db_connection():
+    def __init__(self):
+        conn_str = os.getenv("BETS_DB_CONN_STR")
+        self.conn = psycopg2.connect(conn_str)
+    def get_cursor(self):
+        return self.conn.cursor()
+    def commit(self):
+        self.conn.commit()
+
+db = db_connection()
+
+def get_games():
+    with db.get_cursor() as cursor:
+        cursor.execute("SELECT * FROM games;")
+        return cursor.fetchall()
+
+def place_bet(game_id, bet_amount, bet_on):
+    with db.get_cursor() as cursor:
+        try:
+            query = """
+                INSERT INTO bets (game_id, bet_amount, team_bet_on) VALUES
+                (%s, %s, %s);
+            """
+            cursor.execute(query, (game_id, bet_amount, bet_on))
+            db.commit()
+        except Exception as e:
+            print(e)
