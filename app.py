ae6a2154d20ee7029944bd7e9d7b0d8f758e6bd3
marwankhalil <marwank@vt.edu>
2025-03-25 20:44:51 -0400

    add port

--- app.py
+++ app.py
@@ -92,3 +92,7 @@ def update_matches_to_in_progress():
 def complete_matches():
     response, status_code = complete_matches_controller()
     return jsonify(response), status_code
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
